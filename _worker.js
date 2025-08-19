2eef64ea7efac38ab69c748412152ad80a145e41
ovovoyipoq46 <ovovoyipoq46@gmail.com>
2025-08-19 00:30:25 -0700

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -3,7 +3,7 @@
 import { connect } from 'cloudflare:sockets';
 
 // 建议修改为自己的 UUID
-let userID = 'd342d11e-d424-4583-b36e-524ab1f0afa4';
+let userID = 'b9175b2c-4cdc-4033-8304-676512edd69c';
 
 // 生成配置文件的 CF 优选 IP
 const bestCFIP = "www.gov.se"
